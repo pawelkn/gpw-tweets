a6208d0f01b6586c42f993c69b783bda70d651d9
Paweł Knioła <pawel.kn@gmail.com>
2022-06-03 09:14:15 +0200

    Separate triggered stocks by candle

--- src/volume-notifier.ts
+++ src/volume-notifier.ts
@@ -1,5 +1,3 @@
-'use strict'
-
 import * as fs from 'fs'
 import * as nodemailer from 'nodemailer'
 import WSEQuotes from './wse-quotes'
@@ -16,30 +14,37 @@ const polishStocks: PolishStocks = JSON.parse(fs.readFileSync(polishStocksFile, 
 const smtpConfig: SmtpConfig = JSON.parse(fs.readFileSync(smtpConfigFile, { encoding: 'utf8', flag: 'r' }))
 const mailingList: MailingList = JSON.parse(fs.readFileSync(mailingListFile, { encoding: 'utf8', flag: 'r' }))
 
-const volume_threshold = process.env.VOLUME_NOTIFIER_VOLUME_THRESHOLD || 2.0
-const min_turnover = process.env.VOLUME_NOTIFIER_MIN_TURNOVER || 50_000
-const min_price = process.env.VOLUME_NOTIFIER_MIN_PRICE || 2.0
+const volumeRise = process.env.VOLUME_NOTIFIER_VOLUME_RISE || 2.0
+const minTurnover = process.env.VOLUME_NOTIFIER_MIN_TURNOVER || 50_000
+const minPrice = process.env.VOLUME_NOTIFIER_MIN_PRICE || 2.0
+
+type Triggered = { white: string[], black: string[], doji: string[] }
+let triggered: Triggered = { white: [], black: [], doji: [] }
 
 const wseQuotes = new WSEQuotes()
 wseQuotes.update()
     .then(() => getTriggered()
-        .then((triggered) => sendMail(triggered)))
+        .then(() => sendMail()))
 
 async function getTriggered() {
-    let triggered: string[] = []
     for (const stock of polishStocks) {
         try {
             const hist = await wseQuotes.getHistoricalPrices(stock.name)
             if (hist.length > 2) {
                 const last = hist[hist.length - 1]
-                const last_avg = (last.open + last.high + last.low + last.close) / 4
-                const last_turnover = last_avg * last.volume
+                const lastAvg = (last.open + last.high + last.low + last.close) / 4
+                const lastTurnover = lastAvg * last.volume
 
-                if ((last.volume / hist[hist.length - 2].volume > volume_threshold) &&
-                    (last_turnover > min_turnover) &&
-                    (last.close > min_price)
+                if ((last.volume / hist[hist.length - 2].volume > volumeRise) &&
+                    (lastTurnover > minTurnover) &&
+                    (last.close > minPrice)
                 ) {
-                    triggered.push(stock.name)
+                    if (last.close == last.open)
+                        triggered.doji.push(stock.name)
+                    else if (last.close > last.open)
+                        triggered.white.push(stock.name)
+                    else
+                        triggered.black.push(stock.name)
                 }
             }
         } catch (e) {
@@ -47,11 +52,11 @@ async function getTriggered() {
         }
     }
 
-    console.log('Triggered', triggered)
-    return triggered
+    for (const candle in triggered)
+        console.log('Triggered', candle, triggered[candle as keyof Triggered])
 }
 
-async function sendMail(triggered: string[]) {
+async function sendMail() {
     const transporter = nodemailer.createTransport({
         host: smtpConfig.host,
         port: smtpConfig.port,
@@ -64,17 +69,30 @@ async function sendMail(triggered: string[]) {
 
     const info = await transporter.sendMail({
         from: `AutoSender <${smtpConfig.sender}>`,
-        to: mailingList.join(", "),
+        to: mailingList.join(', '),
         subject: 'Volume notification',
-        html: `Parameters:
-            <ul>
-                <li>volume threshold: ${volume_threshold}</li>
-                <li>min turnover: ${min_turnover}</li>
-                <li>min price: ${min_price}</li>
-            </ul>
-            <a href='https://stockaggregator.com?tickers=${triggered.join("%20")}'>${triggered.join(" ")}</a>
-        `
+        html: getHtmlMessage()
     })
 
-    console.log("Message sent: %s", info.messageId)
+    console.log('Message sent: %s', info.messageId)
+}
+
+function getHtmlMessage() {
+    let html = `Parameters:
+        <ul>
+            <li>volume rise: ${volumeRise}</li>
+            <li>min turnover: ${minTurnover}</li>
+            <li>min price: ${minPrice}</li>
+        </ul>
+    `
+
+    for (const candle in triggered) {
+        const stockNames = triggered[candle as keyof Triggered]
+
+        html += `<p>Triggered ${candle}: `
+        html += stockNames.length === 0 ? 'nothing' : `<a href='https://stockaggregator.com?tickers=${stockNames.join("%20")}'>${stockNames.join(" ")}</a>`
+        html += '</p>'
+    }
+
+    return html
 }
