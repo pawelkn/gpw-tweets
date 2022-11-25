822295dae0dc6c6959d04aa7f0d0618e7c26b491
Paweł Knioła <pawel.kn@gmail.com>
2022-11-25 08:05:53 +0100

    WSE Quotes: Add a resample method

--- src/wse-quotes.ts
+++ src/wse-quotes.ts
@@ -52,4 +52,41 @@ export default class WSEQuotes {
             })
         })
     }
+
+    public resample(data: Candlestick[], period: string = 'D') {
+        switch(period) {
+        case 'D':
+            return data
+
+        case 'W':
+            let candlesticks: Candlestick[] = []
+            let lastYear: number = NaN
+            let lastWeek: number = NaN
+
+            for (const d of data) {
+                const day = new Date(Date.UTC(+d.date.substring(0, 4), +d.date.substring(4, 6) - 1, +d.date.substring(6, 8)))
+                const yearStart = new Date(Date.UTC(day.getUTCFullYear(), 0, 1))
+                const week = Math.ceil((((day.getTime() - yearStart.getTime()) / 86400000) + yearStart.getDay() + 1) / 7)
+
+                if ((lastWeek !== week) && (isNaN(lastYear) || isNaN(lastWeek) || (yearStart.getDay() === 1) || (lastYear === day.getUTCFullYear()))) {
+                    candlesticks.push(d)
+                }
+                else {
+                    const last = candlesticks[candlesticks.length - 1]
+                    if (last.low > d.low) last.low = d.low
+                    if (last.high < d.high) last.high = d.high
+                    last.date = d.date
+                    last.close = d.close
+                    last.volume += d.volume
+                }
+
+                lastYear = day.getUTCFullYear()
+                lastWeek = week
+            }
+            return candlesticks
+
+        default:
+            return []
+        }
+    }
 }
--- test/wse-quotes.test.ts
+++ test/wse-quotes.test.ts
@@ -18,4 +18,49 @@ describe('wse-quotes', () => {
             assert.equal(day.volume, 1885)
         })
     })
+
+    describe('#resample()', () => {
+        it('should resample candlesticks', async () => {
+            const wseQuotes = new WseQuotes('./test')
+            let data = await wseQuotes.getHistorical('11BIT')
+            data = wseQuotes.resample(data, 'W')
+            assert.ok(data.length > 0)
+
+            let week = data.find((d => d.date === '20220606'))
+            assert.ok(week !== undefined)
+
+            assert.equal(week.open, 520.0)
+            assert.equal(week.high, 531.0)
+            assert.equal(week.low, 510.0)
+            assert.equal(week.close, 518.0)
+            assert.equal(week.volume, 1885)
+
+            week = data.find((d => d.date === '20220603'))
+            assert.ok(week !== undefined)
+
+            assert.equal(week.open, 489.0)
+            assert.equal(week.high, 543.0)
+            assert.equal(week.low, 481.5)
+            assert.equal(week.close, 522.0)
+            assert.equal(week.volume, 14049)
+
+            week = data.find((d => d.date === '20171229'))
+            assert.ok(week !== undefined)
+
+            assert.equal(week.open, 191.0)
+            assert.equal(week.high, 198.0)
+            assert.equal(week.low, 187.1)
+            assert.equal(week.close, 198)
+            assert.equal(week.volume, 20054)
+
+            week = data.find((d => d.date === '20150102'))
+            assert.ok(week !== undefined)
+
+            assert.equal(week.open, 64.58)
+            assert.equal(week.high, 76.60)
+            assert.equal(week.low, 64.05)
+            assert.equal(week.close, 72.00)
+            assert.equal(week.volume, 82512)
+        })
+    })
 })
