c3f9252070197fd8d45eacd50b7f7fd6d574ed13
Paweł Knioła <pawel.kn@gmail.com>
2022-11-26 14:55:56 +0100

    Candlestick: Code refactoring

--- src/candlestick.ts
+++ src/candlestick.ts
@@ -17,37 +17,37 @@ export default class Candlestick {
         this.volume = volume !== undefined ? volume : NaN
     }
 
-    public isBullish = () => this.open < this.close
-    public isBearish = () => this.open > this.close
+    public isBullish () { return this.open < this.close }
+    public isBearish () { return this.open > this.close }
 
-    public bodyLen = () => abs(this.open - this.close)
-    public wickLen = () => this.high - max(this.open, this.close)
-    public tailLen = () => min(this.open, this.close) - this.low
+    public bodyLen () { return abs(this.open - this.close) }
+    public wickLen () { return this.high - max(this.open, this.close) }
+    public tailLen () { return min(this.open, this.close) - this.low }
 
-    public isHammer = (ratio = 2) => this.tailLen() > (this.bodyLen() * ratio) && this.wickLen() < this.bodyLen()
-    public isInvertedHammer = (ratio = 2) => this.wickLen() > (this.bodyLen() * ratio) && this.tailLen() < this.bodyLen()
-    public isBullishHammer = () => this.isBullish() && this.isHammer()
-    public isBearishHammer = () => this.isBearish() && this.isHammer()
-    public isBullishInvertedHammer = () => this.isBullish() && this.isInvertedHammer()
-    public isBearishInvertedHammer = () => this.isBearish() && this.isInvertedHammer()
+    public isHammer (ratio = 2) { return this.tailLen() > (this.bodyLen() * ratio) && this.wickLen() < this.bodyLen() }
+    public isInvertedHammer (ratio = 2) { return this.wickLen() > (this.bodyLen() * ratio) && this.tailLen() < this.bodyLen() }
+    public isBullishHammer () { return this.isBullish() && this.isHammer() }
+    public isBearishHammer () { return this.isBearish() && this.isHammer() }
+    public isBullishInvertedHammer () { return this.isBullish() && this.isInvertedHammer() }
+    public isBearishInvertedHammer () { return this.isBearish() && this.isInvertedHammer() }
 
-    public hasGapUp = (previous: Candlestick) => previous.high < this.low
-    public hasGapDown = (previous: Candlestick) => previous.low > this.high
+    public hasGapUp (previous: Candlestick) { return previous.high < this.low }
+    public hasGapDown (previous: Candlestick) { return previous.low > this.high }
 
-    public isHangingMan = (previous: Candlestick) => previous.isBullish() && this.isBearishHammer() && this.hasGapUp(previous)
-    public isShootingStar = (previous: Candlestick) => previous.isBullish() && this.isInvertedHammer() && this.hasGapUp(previous)
-    public isMorningStar = (previous: Candlestick) => previous.isBearish() && this.isHammer() && this.hasGapDown(previous)
-    public isBullishEngulfing = (previous: Candlestick) => previous.isBearish() && this.isBullish() && this.isEngulfed(previous)
-    public isBearishEngulfing = (previous: Candlestick) => previous.isBullish() && this.isBearish() && this.isEngulfed(previous)
-    public isBullishHarami = (previous: Candlestick) => previous.isBullish() && this.isBullish() && this.isEngulfed(previous)
-    public isBearishHarami = (previous: Candlestick) => previous.isBullish() && this.isBullish() && this.isEngulfed(previous)
-    public isBullishKicker = (previous: Candlestick) => previous.isBearish() && this.isBullish() && this.hasGapUp(previous) && !(this.isHammer() || this.isInvertedHammer())
-    public isBearishKicker = (previous: Candlestick) => previous.isBullish() && this.isBearish() && this.hasGapDown(previous) && !(this.isHammer() || this.isInvertedHammer())
-    public isBullishGap = (previous: Candlestick) => this.isBullish() && this.hasGapUp(previous)
-    public isBearishGap = (previous: Candlestick) => this.isBearish() && this.hasGapDown(previous)
-    public isBullishSmash = (previous: Candlestick) => previous.isInvertedHammer() && this.isBullish() && (this.close > previous.high)
-    public isBearishSmash = (previous: Candlestick) => previous.isHammer() && this.isBearish() && (this.close < previous.low)
+    public isHangingMan (previous: Candlestick) { return previous.isBullish() && this.isBearishHammer() && this.hasGapUp(previous) }
+    public isShootingStar (previous: Candlestick) { return previous.isBullish() && this.isInvertedHammer() && this.hasGapUp(previous) }
+    public isMorningStar (previous: Candlestick) { return previous.isBearish() && this.isHammer() && this.hasGapDown(previous) }
+    public isBullishEngulfing (previous: Candlestick) { return previous.isBearish() && this.isBullish() && this.isEngulfed(previous) }
+    public isBearishEngulfing (previous: Candlestick) { return previous.isBullish() && this.isBearish() && this.isEngulfed(previous) }
+    public isBullishHarami (previous: Candlestick) { return previous.isBullish() && this.isBullish() && this.isEngulfed(previous) }
+    public isBearishHarami (previous: Candlestick) { return previous.isBullish() && this.isBullish() && this.isEngulfed(previous) }
+    public isBullishKicker (previous: Candlestick) { return previous.isBearish() && this.isBullish() && this.hasGapUp(previous) && !(this.isHammer() || this.isInvertedHammer()) }
+    public isBearishKicker (previous: Candlestick) { return previous.isBullish() && this.isBearish() && this.hasGapDown(previous) && !(this.isHammer() || this.isInvertedHammer()) }
+    public isBullishGap (previous: Candlestick) { return this.isBullish() && this.hasGapUp(previous) }
+    public isBearishGap (previous: Candlestick) { return this.isBearish() && this.hasGapDown(previous) }
+    public isBullishSmash (previous: Candlestick) { return previous.isInvertedHammer() && this.isBullish() && (this.close > previous.high) }
+    public isBearishSmash (previous: Candlestick) { return previous.isHammer() && this.isBearish() && (this.close < previous.low) }
 
-    private bodyEnds = () => this.open <= this.close ? { bottom: this.open, top: this.close } : { bottom: this.close, top: this.open }
-    private isEngulfed = (previous: Candlestick) => previous.bodyEnds().top <= this.bodyEnds().top && previous.bodyEnds().bottom >= this.bodyEnds().bottom
+    private bodyEnds () { return this.open <= this.close ? { bottom: this.open, top: this.close } : { bottom: this.close, top: this.open } }
+    private isEngulfed (previous: Candlestick) { return previous.bodyEnds().top <= this.bodyEnds().top && previous.bodyEnds().bottom >= this.bodyEnds().bottom }
 }
