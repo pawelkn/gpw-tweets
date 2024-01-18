7a31feaf8915d335224096eb79b00bcb60a6675c
Paweł Knioła <pawel.kn@gmail.com>
2024-01-18 10:45:55 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,15 @@ fi
 docker build -t gpw-tweets . &&
 docker save gpw-tweets | bzip2 | pv | ssh $1 '
     docker load
+    docker volume create mstall
+    docker volume create images
     docker service rm gpw-tweets
     docker service create \
         --name gpw-tweets \
         --secret twitter-credentials \
         -e GPW_TWEETS_TWITTER_CREDENTIALS_FILE=/run/secrets/twitter-credentials \
+        --mount source=mstall,target=/app/mstall \
+        --mount source=images,target=/app/images \
         --read-only \
         gpw-tweets
 '
